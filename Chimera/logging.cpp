ecaa85b2d93fbd71623d72840d946700f43d3d1c
Brandon Braun <brandonbraun653@gmail.com>
2018-06-21 10:34:08 -0500

    Added super simple colorized logging class

---
+++ Chimera/logging.cpp
@@ -0,0 +1,72 @@
+
+
+/* Chimera Includes */
+#include <Chimera/logging.hpp>
+
+namespace Chimera
+{
+	namespace Log
+	{
+		SimpleLogger Console;
+
+		void SimpleLogger::applyLogLevelColoring(Level level)
+		{
+			switch (level)
+			{
+			case Level::INFO:
+				printf(Colors::BRIGHT_GREEN);
+				break;
+
+			case Level::WARN:
+				printf(Colors::BRIGHT_YELLOW);
+				break;
+				
+			case Level::ERROR:
+				printf(Colors::BRIGHT_MAGENTA);
+				break;
+
+			case Level::FATAL:
+				printf(Colors::BRIGHT_RED);
+				break;
+				
+			case Level::DBG:
+				printf(Colors::BRIGHT_BLUE);
+				break;
+
+			default: 
+				printf(Colors::RESET);
+				break;
+			}
+		}
+
+		void SimpleLogger::applyLogTag(Level level)
+		{
+			switch (level)
+			{
+			case Level::INFO:
+				printf("[INFO]\t");
+				break;
+
+			case Level::WARN:
+				printf("[WARN]\t");
+				break;
+
+			case Level::ERROR:
+				printf("[ERROR]\t");
+				break;
+
+			case Level::FATAL:
+				printf("[FATAL]\t");
+				break;
+
+			case Level::DBG:
+				printf("[DEBUG]\t");
+				break;
+
+			default:
+				printf("[]\t");
+				break;
+			}
+		}
+	}
+}
--- Chimera/logging.hpp
+++ Chimera/logging.hpp
@@ -2,26 +2,78 @@
 #ifndef CHIMERA_LOGGING_HPP
 #define CHIMERA_LOGGING_HPP
 
-#include <Chimera/lib/spdlog/include/spdlog/spdlog.h>
-#include <Chimera/lib/spdlog/include/spdlog/sinks/sink.h>
+/* C/C++ Includes */
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <memory>
 
+/* Chimera Includes */
+#include <Chimera/serial.hpp>
 
-class my_sink : public spdlog::sinks::sink
+namespace Chimera
 {
-	void log(const spdlog::details::log_msg& msg) override
+	namespace Log
 	{
-		// Your code here. 
-		// details::log_msg is a struct containing the log entry info like level, timestamp, thread id etc.
-		// msg.formatted contains the formatted log.
-		// msg.raw contains pre formatted log
-		//std::cout << msg.formatted.str();
-	}
+		struct Colors
+		{
+			static constexpr const char* RESET		= "\x1B[0m";
+			static constexpr const char* BLACK		= "\x1B[30m";
+			static constexpr const char* RED		= "\x1B[31m";
+			static constexpr const char* GREEN		= "\x1B[32m";
+			static constexpr const char* YELLOW		= "\x1B[33m";
+			static constexpr const char* BLUE		= "\x1B[34m";
+			static constexpr const char* MAGENTA	= "\x1B[35m";
+			static constexpr const char* CYAN		= "\x1B[36m";
+			static constexpr const char* WHITE		= "\x1B[37m";
+			
+			static constexpr const char* BRIGHT_BLACK	= "\x1B[90m";
+			static constexpr const char* BRIGHT_RED		= "\x1B[91m";
+			static constexpr const char* BRIGHT_GREEN	= "\x1B[92m";
+			static constexpr const char* BRIGHT_YELLOW	= "\x1B[93m";
+			static constexpr const char* BRIGHT_BLUE	= "\x1B[94m";
+			static constexpr const char* BRIGHT_MAGENTA = "\x1B[95m";
+			static constexpr const char* BRIGHT_CYAN	= "\x1B[96m";
+			static constexpr const char* BRIGHT_WHITE	= "\x1B[97m";
+		};
 
-	void flush()
-	{
-		//std::cout << std::flush;
-	}
+		/** Available options for tagging a log message with its intended level */
+		enum class Level
+		{
+			INFO,
+			WARN,
+			ERROR,
+			FATAL,
+			DBG,
+			MAX_LOG_LEVELS
+		};
 
-};
+		/** An extremely simple logging class that directly writes a console through printf(). 
+		 *	The only features are to colorize the output and include a logging level tag. Other than that,
+		 *	it is simply just plain'ol printf().
+		 **/
+		class SimpleLogger
+		{
+		public:
+			template<typename ...Args>
+			void log(Level level, const char* format, Args const& ... args)
+			{
+				applyLogLevelColoring(level);
+				applyLogTag(level);
+				printf(format, args...);
+				printf(Colors::RESET);
+			}
+
+			SimpleLogger() = default;
+			~SimpleLogger() = default;
+
+		private:
+			void applyLogLevelColoring(Level level);
+			void applyLogTag(Level level);
+		};
+
+		extern SimpleLogger Console;
+	}
+}
 
 #endif
