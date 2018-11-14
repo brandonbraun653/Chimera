cb21b0e96281de8115ab6cf6f85868a4932a0e2a
Brandon Braun <brandonbraun653@gmail.com>
2018-11-13 22:13:13 -0600

    Added an SPI interface that all low level drivers should adhere to
    Issue: 147

---
+++ Chimera/interface.hpp
@@ -0,0 +1,287 @@
+#ifndef CHIMERA_INTERFACE_HPP
+#define CHIMERA_INTERFACE_HPP
+
+#include <stdlib.h>
+#include <cstdint>
+
+#include <Chimera/types.hpp>
+#include <Chimera/preprocessor.hpp>
+
+namespace Chimera
+{
+    namespace SPI
+    {
+        class Interface
+        {
+        public:
+
+            /**
+             *  @brief Initializes the SPI hardware according to the setup struct
+             *
+             *  @param[in]   setupStruct     Contains information on how to initialize SPI
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status init(const Chimera::SPI::Setup& setupStruct) = 0;
+
+            /**
+             *  @brief Sets the chip select GPIO to a logical state
+             *
+             *  @param[in]   value   The state to set the chip select to
+             */
+            virtual Chimera::SPI::Status setChipSelect(const Chimera::GPIO::State &value) = 0;
+
+            /**
+             *  @brief Instruct the chip select to behave in a specific manner
+             *
+             *  @param[in]  mode    The desired mode for the chip select to operate in
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status setChipSelectControlMode(const Chimera::SPI::ChipSelectMode &mode) = 0;
+
+            /**
+             *  @brief Writes data onto the SPI bus
+             *
+             *  @param[in]   txBuffer    Data buffer to be sent
+             *  @param[in]   length      Number of bytes to be sent
+             *  @param[in]   disableCS   Optionally disable the chip select line after transmission complete
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status writeBytes(const uint8_t *const txBuffer, size_t length,
+                const bool &disableCS = true, const bool &autoRelease = false, uint32_t timeoutMS = 10) = 0;
+
+            /**
+             *  @brief Write multiple buffers of data onto the bus
+             *
+             *  This is useful when you want to queue up multiple transmissions but the calling program
+             *  needs to go off and do something else instead of managing the transfers.
+             *
+             *
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status queueTransfers(const std::vector<uint8_t *const> &txBuffers)
+            {
+                //TODO: This likely will need to turn into a struct with transfer specific information
+
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Write multiple buffers of data onto the bus
+             *
+             *  This is useful when you want to queue up multiple transmissions but the calling program
+             *  needs to go off and do something else instead of managing the transfers.
+             *
+             *
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status queueTransfers(const uint8_t **const txBuffers)
+            {
+                //TODO: Same as other, but with an array of the struct type (more memory efficient)
+
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Write data to the slave queue that will be clocked out on the next transfer
+             *
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status writeSlaveQueue(const uint8_t *const txBuffer, size_t length)
+            {
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Reads data from the SPI bus
+             *
+             *  Internally,the function will call readWriteBytes() and use the rxBuffer as the source of
+             *  bytes to transmit on the MOSI line.
+             *
+             *  Master
+             *  ==================
+             *  This section describes the behavior of readBytes() when SPI is configured as a master.
+             *
+             *      Blocking Mode
+             *      ------------------
+             *      When called in blocking mode, the SPI hardware will attempt to read a number of bytes from the bus
+             *      with an enforced timeout. Should the requested number of bytes not arrive before the timeout expires,
+             *      an error code is returned indicating the timeout.
+             *
+             *      Interrupt Mode
+             *      ------------------
+             *      When using interrupt mode, an ISR handles the transfer. If supported by the underlying driver, the
+             *      user can attach callbacks to be notified when the transfer is complete [onReadCompleteCallback()] or
+             *      when an error occurs [onErrorCallback()].
+             *
+             *      DMA Mode
+             *      ------------------
+             *      The DMA hardware will handle the transfer. If supported by the underlying driver, the
+             *      user can attach callbacks to be notified when the transfer is complete [onReadCompleteCallback()] or
+             *      when an error occurs [onErrorCallback()].
+             *
+             *
+             *  Slave
+             *  ==================
+             *  This section describes the behavior of readBytes() when SPI is configured as a slave.
+             *
+             *      Blocking Mode
+             *      ------------------
+             *      When called in blocking mode, the SPI hardware will attempt to read a number of bytes from MOSI
+             *      with an enforced timeout. The corresponding bytes clocked out on MISO will be any data contained
+             *      in the receive buffer before the transfer starts. Should the requested number of bytes not arrive
+             *      before the timeout expires or the slave select line is not asserted, an error code is returned
+             *      indicating the timeout.
+             *
+             *      Interrupt Mode
+             *      ------------------
+             *
+             *
+             *      DMA Mode
+             *      ------------------
+             *
+             *
+             *  @param[out]  rxBuffer    Data buffer to read into
+             *  @param[in]   length      Number of bytes to read
+             *  @param[in]   disableCS   Optionally disable the chip select line after transmission complete
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status readBytes(uint8_t * const rxBuffer, size_t length,
+                const bool & disableCS = true, const bool &autoRelease = false, uint32_t timeoutMS = 10) = 0;
+
+            /**
+             *  @brief Reads a number of bytes out from the internal slave receive buffer
+             *
+             *  @param[in]   rxBuffer    Data buffer to read into
+             *  @param[in]   length      Number of bytes to read
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status readSlaveQueue(uint8_t *const rxBuffer, size_t length)
+            {
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Transmits and receives data on the SPI bus
+             *
+             *  @param[in]   txBuffer    Data buffer to write from
+             *  @param[out]  rxBuffer    Data buffer to read into
+             *  @param[in]   length      Number of bytes to transfer
+             *  @param[in]   disableCS   Optionally disable the chip select line after transmission complete
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status readWriteBytes(const uint8_t * const txBuffer, uint8_t * const rxBuffer, size_t length,
+                const bool& disableCS = true, const bool &autoRelease = false, uint32_t timeoutMS = 10) = 0;
+
+            /**
+             *  @brief Set the hardware peripheral operational mode.
+             *  This should allow the user to choose if the hardware operates in blocking, interrupt, or DMA mode
+             *
+             *  @param[in]   periph      The peripheral to set the behavior on
+             *  @param[in]   mode        Desired operational mode of the peripheral
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status setPeripheralMode(const Chimera::SPI::SubPeripheral &periph, const Chimera::SPI::SubPeripheralMode &mode) = 0;
+
+            /**
+             *  @brief Change the frequency of the SPI output clock during runtime
+             *
+             *  @param[in]   freq    Desired SPI clock frequency in Hz
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status setClockFrequency(const uint32_t &freq) = 0;
+
+            /**
+             *  @brief Get the current SPI clock frequency
+             *
+             *  @param[out]  freq    Reported SPI clock
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status getClockFrequency(uint32_t * const freq) = 0;
+
+            /**
+             *  @brief Reserves the SPI hardware to allow unobstructed use
+             *
+             *  @param[in]  timeout_ms  How many milliseconds to wait for the hardware to become available
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status reserve(const uint32_t &timeout_ms = 0u) = 0;
+
+            /**
+             *  @brief Releases a previous reservation
+             *
+             *  @param[in]  timeout_ms  How many milliseconds to wait for the hardware to release
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status release(const uint32_t &timeout_ms = 0u ) = 0;
+
+            /**
+             *  @brief Allows the user to assign a callback function to the write complete event
+             *
+             *  @param[in]  func  Callback function
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status onWriteCompleteCallback(const Chimera::void_func_void func)
+            {
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Allows the user to assign a callback function to the read complete event
+             *
+             *  @param[in]  func  Callback function
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status onReadCompleteCallback(const Chimera::void_func_void func)
+            {
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Allows the user to assign a callback function to the read-write complete event
+             *
+             *  @param[in]  func  Callback function
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status onReadWriteCompleteCallback(const Chimera::void_func_void func)
+            {
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+            /**
+             *  @brief Allows the user to assign a callback function on an error event
+             *
+             *  The function will be passed an error code indicating what happened
+             *
+             *  @param[in]  func  Callback function
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status onErrorCallback(const Chimera::void_func_uint32_t func)
+            {
+                return Chimera::SPI::Status::NOT_SUPPORTED;
+            }
+
+#if defined(USING_FREERTOS)
+
+            /**
+             *  @brief Allows the user to have a semaphore given to when an event occurs
+             *
+             *  @param[in]  event   The event to be waiting on
+             *  @param[in]  semphr  The semaphore to be given to upon event occurance
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status attachEventWakeup(const Chimera::FreeRTOS::SPIEvent &event, const SemaphoreHandle_t *const semphr) = 0;
+
+            /**
+             *  @brief Removes a semaphore from the event wakeup list
+             *
+             *  @param[in]  semphr  The semaphore to be removed
+             *  @return Chimera::SPI::Status
+             */
+            virtual Chimera::SPI::Status removeEventWakeup(const SemaphoreHandle_t *const semphr) = 0;
+
+            #endif /* !USING_FREERTOS */
+        };
+    }
+}
+
+#endif /* !CHIMERA_INTERFACE_HPP */
--- Chimera/spi.hpp
+++ Chimera/spi.hpp
@@ -16,70 +16,20 @@ namespace Chimera
 		class SPIClass : public CHIMERA_INHERITED_SPI
 		{
 		public:
-			SPI::Status begin(const Setup& setupStruct)
-			{
-				return spi->cbegin(setupStruct);
-			}
 
-			SPI::Status write(uint8_t* data_in, size_t length, const bool& ssDisableAfterTX = true)
-			{
-				return spi->cwrite(data_in, length, ssDisableAfterTX);
-			}
 			
-			SPI::Status write(uint8_t* data_in, uint8_t* data_out, size_t length, const bool& ssDisableAfterTX = true)
+			SPIClass(const int& channel) : CHIMERA_INHERITED_SPI( channel )
 			{
-				return spi->cwrite(data_in, data_out, length, ssDisableAfterTX);
-			}
 
-			SPI::Status setMode(SubPeripheral periph, SubPeripheralMode mode)
-			{
-				return spi->csetMode(periph, mode);
-			}
-			
-			SPI::Status updateClockFrequency(uint32_t freq)
-			{
-				return spi->cupdateClockFrequency(freq);
-			}
-
-			uint32_t getClockFrequency()
-			{
-				return spi->cgetClockFrequency();
-			}
-
-			void writeSS(Chimera::GPIO::State value)
-			{
-				spi->cwriteSS(value);
-			}
-			
-			SPIClass(const int& channel)
-			{
-				#if defined(USING_BOOST)
-				if (CHIMERA_INHERITED_SPI::usesBoost)
-				{
-					auto boost_spi = CHIMERA_INHERITED_SPI::create(channel);
-					this->spi = Chimera::convert_shared_ptr(boost_spi);
-				}
-				#else
-				this->spi = CHIMERA_INHERITED_SPI::create(channel);
-				#endif
-				
 			};
 
 			~SPIClass() = default;
 
 		private:
-			std::shared_ptr<CHIMERA_INHERITED_SPI> spi;
+
 		};
 		typedef std::shared_ptr<SPIClass> SPIClass_sPtr;
 		typedef std::unique_ptr<SPIClass> SPIClass_uPtr;
-		
-		CLASS_METHOD_CHECKER(has_cbegin, CHIMERA_INHERITED_SPI, cbegin, Status, const Setup&);
-		CLASS_METHOD_CHECKER(has_cwrite1, CHIMERA_INHERITED_SPI, cwrite, Status, uint8_t*, size_t, const bool&);
-		CLASS_METHOD_CHECKER(has_cwrite2, CHIMERA_INHERITED_SPI, cwrite, Status, uint8_t*, uint8_t*, size_t, const bool&);
-		CLASS_METHOD_CHECKER(has_csetMode, CHIMERA_INHERITED_SPI, csetMode, Status, SubPeripheral, SubPeripheralMode);
-		CLASS_METHOD_CHECKER(has_cupdateClockFrequency, CHIMERA_INHERITED_SPI, cupdateClockFrequency, Status, uint32_t);
-		CLASS_METHOD_CHECKER(has_cwritess, CHIMERA_INHERITED_SPI, cwriteSS, void, Chimera::GPIO::State);
-		CLASS_METHOD_CHECKER(has_cgetClockFrequency, CHIMERA_INHERITED_SPI, cgetClockFrequency, uint32_t, void);
 	}
 }
 
