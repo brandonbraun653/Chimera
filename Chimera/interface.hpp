2e89622c308eb02118dcca81bafab4c50c05149a
Brandon Braun <brandonbraun653@gmail.com>
2019-04-19 08:51:59 -0500

    Added proper return values to SerialUnsupported class.

--- Chimera/interface.hpp
+++ Chimera/interface.hpp
@@ -1028,91 +1028,91 @@ namespace Chimera
 
       virtual Chimera::Status_t assignHW( const uint8_t channel, const IOPins &pins ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t begin( const Modes txMode, const Modes rxMode ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t end() override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t configure( const uint32_t baud, const CharWid width, const Parity parity, const StopBits stop,
                                            const FlowControl flow ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t setBaud( const uint32_t baud ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t setMode( const SubPeripheral periph, const Modes mode ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t write( const uint8_t *const buffer, const size_t length,
                                        const uint32_t timeout_mS = 500 ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t read( uint8_t *const buffer, const size_t length, const uint32_t timeout_mS = 500 ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t flush( const SubPeripheral periph ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::FAIL;
       }
 
       virtual Chimera::Status_t readAsync( uint8_t *const buffer, const size_t len ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Status::NOT_SUPPORTED;
       }
 
       virtual Chimera::Status_t enableBuffering( const SubPeripheral periph,
                                                  boost::circular_buffer<uint8_t> *const buffer ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Status::NOT_SUPPORTED;
       }
 
       virtual Chimera::Status_t disableBuffering( const SubPeripheral periph ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Status::NOT_SUPPORTED;
       }
 
       virtual bool available( size_t *const bytes = nullptr ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return false;
       }
 
       virtual Chimera::Status_t attachEventNotifier( const Event event, volatile bool *const notifier ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::NOT_SUPPORTED;
       }
 
       virtual Chimera::Status_t removeEventNotifier( const Event event, volatile bool *const notifier ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::NOT_SUPPORTED;
       }
 
 #if defined( USING_FREERTOS )
       virtual Chimera::Status_t attachEventNotifier( const Event event, SemaphoreHandle_t *const semphr ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::NOT_SUPPORTED;
       }
 
       virtual Chimera::Status_t removeEventNotifier( const Event event, SemaphoreHandle_t *const semphr ) override
       {
-        throw std::logic_error( "The method or operation is not implemented." );
+        return Chimera::CommonStatusCodes::NOT_SUPPORTED;
       }
 #endif /* USING_FREERTOS */
     };
