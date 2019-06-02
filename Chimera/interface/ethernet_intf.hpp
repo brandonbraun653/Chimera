7df2d45d4e2341f84d12bdca00ee05330725470d
Brandon Braun <brandonbraun653@gmail.com>
2019-06-02 15:25:35 -0500

    Added placeholder file for an ethernet interface spec.

---
+++ Chimera/interface/ethernet_intf.hpp
@@ -0,0 +1,24 @@
+/********************************************************************************
+ *  File Name:
+ *    ethernet_intf.hpp
+ *
+ *  Description:
+ *    Ethernet hardware driver interface specification for Chimera backends
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#pragma once
+#ifndef CHIMERA_ETHERNET_INTERFACE_HPP
+#define CHIMERA_ETHERNET_INTERFACE_HPP
+
+namespace Chimera::Ethernet
+{
+  class Interface
+  {
+  public:
+    
+  };
+}
+
+#endif /* !CHIMERA_ETHERNET_INTERFACE_HPP */
