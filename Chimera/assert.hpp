43dd5fd65fdb7c21caacc9469b3c6ebff7b31faf
Brandon Braun <brandonbraun653@gmail.com>
2019-06-23 09:38:56 -0500

    Added new assert file. Mostly just a placeholder for now, but it does include one useful macro for embedded debugging.

---
+++ Chimera/assert.hpp
@@ -0,0 +1,25 @@
+/********************************************************************************
+ *   File Name:
+ *    assert.hpp
+ *
+ *   Description:
+ *    Lightweight exception definitions
+ *
+ *   2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#pragma once
+#ifndef CHIMERA_ASSERT_HPP
+#define CHIMERA_ASSERT_HPP
+
+namespace Chimera::Assert
+{
+
+#if defined( DEBUG) && ( DEBUG == 1 )
+  #define DBG_ASSERT(x) if(!(x)){ asm("bkpt 255"); }
+#else 
+  #define DBG_ASSERT(x) (void(0))
+  #endif
+}  // namespace Chimera::Assert
+
+#endif /* !CHIMERA_ASSERT_HPP */
