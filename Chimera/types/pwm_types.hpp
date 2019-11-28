4bee378c11602b205ded12b7b20ed12937c5e55d
Brandon Braun <brandonbraun653@gmail.com>
2019-11-27 20:46:11 -0600

    Added a first cut of a PWM interface spec.

---
+++ Chimera/types/pwm_types.hpp
@@ -0,0 +1,50 @@
+/********************************************************************************
+*   File Name:
+*       pwm_types.hpp
+*
+*   Description:
+*       Type specifications for the Chimera PWM module
+*
+*   2019 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+
+#pragma once
+#ifndef CHIMERA_PWM_TYPES_HPP
+#define CHIMERA_PWM_TYPES_HPP
+
+/* C++ Includes */
+#include <cstdint>
+
+/* Chimera Includes */
+#include <Chimera/types/gpio_types.hpp>
+
+namespace Chimera::PWM
+{
+  /**
+   *  Configuration structure for the PWMInterface 
+   */ 
+  struct Config
+  {
+    size_t channel;                   /**< The hardware PWM channel to use */
+    size_t dutyCycle;                 /**< Initial duty cycle ([0-100])*/
+    size_t period;                    /**< Initial period (uS) */
+    Chimera::GPIO::PinInit gpioInit;  /**< Output GPIO configuration data */
+  };
+
+  /**
+   *  Describes the supported PWM edge types
+   */
+  enum Edge : size_t 
+  {
+    RISING_EDGE,
+    FALLING_EDGE,
+    BOTH_EDGES
+  };
+
+  /**
+   *  Function signature for edge notificatino callbacks
+   */ 
+  using EdgeCallback = void(*)(void);
+}
+
+#endif  /* CHIMERA_PWM_TYPES_HPP */
