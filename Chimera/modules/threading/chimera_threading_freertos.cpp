1c4243b6760f80fb0e8ed19acedadf637766c1d7
Brandon Braun <brandonbraun653@gmail.com>
2020-01-20 19:30:47 -0600

    Reworked the static library project to compile correctly for STM32F446RE

--- Chimera/modules/threading/chimera_threading_freertos.cpp
+++ Chimera/modules/threading/chimera_threading_freertos.cpp
@@ -32,57 +32,6 @@ namespace Chimera::Threading
   {
     taskYIELD();
   }
-
-  /*------------------------------------------------
-  Recursive Mutex Specialization
-  ------------------------------------------------*/
-  template<>
-  RecursiveMutex createSyncObject()
-  {
-    return xSemaphoreCreateRecursiveMutex();
-  }
-
-  template<>
-  void destroySyncObject( RecursiveMutex mutex )
-  {
-    vPortFree( mutex );
-  }
-
-  template<>
-  bool lock( RecursiveMutex mutex )
-  {
-    return ( xSemaphoreTakeRecursive( mutex, portMAX_DELAY ) == pdPASS );
-  }
-
-  template<>
-  bool lockFromISR( RecursiveMutex mutex )
-  {
-    return ( xSemaphoreTakeFromISR( mutex, nullptr ) == pdPASS );
-  }
-
-  template<>
-  bool unlock( RecursiveMutex mutex )
-  {
-    return ( xSemaphoreGiveRecursive( mutex ) == pdPASS );
-  }
-
-  template<>
-  bool unlockFromISR( RecursiveMutex mutex )
-  {
-    return ( xSemaphoreGiveFromISR( mutex, nullptr) == pdPASS );
-  }
-
-
-  /*------------------------------------------------
-  Recursive Timed Mutex Specialization:
-  The underlying type of RecursiveTimedMutex == RecursiveMutex for 
-  FreeRTOS, so we only need to specialize this one instance.
-  ------------------------------------------------*/
-  template<>
-  bool lock( RecursiveTimedMutex mutex, const size_t timeout_ms )
-  {
-    return ( xSemaphoreTakeRecursive( mutex, pdMS_TO_TICKS( timeout_ms ) ) == pdPASS );
-  }
 }
 
 #endif /* CHIMERA_CFG_FREERTOS */
--- Chimera/threading/freertos/freertos_sync_primitives.hpp
+++ Chimera/threading/freertos/freertos_sync_primitives.hpp
@@ -14,10 +14,17 @@
 /* FreeRTOS Includes */
 #include "FreeRTOS.h"
 #include "semphr.h"
+#include "task.h"
 
 namespace Chimera::Threading::detail
 {
-  using native_mutex = SemaphoreHandle_t;
-}
+  using native_mutex                 = SemaphoreHandle_t;
+  using native_recursive_mutex       = SemaphoreHandle_t;
+  using native_timed_mutex           = SemaphoreHandle_t;
+  using native_recursive_timed_mutex = SemaphoreHandle_t;
+
+  using native_thread             = TaskHandle_t;
+  using native_thread_handle_type = TaskHandle_t;
+}  // namespace Chimera::Threading::detail
 
 #endif /* !CHIMERA_THREADING_FREERTOS_TYPES_HPP */
