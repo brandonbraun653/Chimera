e6046d5fbac23962fa42f81c09500a0697776a3d
Brandon Braun <brandonbraun653@gmail.com>
2020-03-20 13:24:42 -0500

    Removed unneeded types

--- Chimera/src/system/system_types.hpp
+++ Chimera/src/system/system_types.hpp
@@ -21,29 +21,24 @@
 
 namespace Chimera::System
 {
-    
-    class SystemControl;
-    using SystemControl_sPtr = std::shared_ptr<SystemControl>;
-    using SystemControl_uPtr = std::unique_ptr<SystemControl>;
+  class SystemControl;
+  using SystemControl_sPtr = std::shared_ptr<SystemControl>;
+  using SystemControl_uPtr = std::unique_ptr<SystemControl>;
 
-    class Identifier;
-    using Identifier_sPtr = std::shared_ptr<Identifier>;
-    using Identifier_uPtr = std::unique_ptr<Identifier>;
+  enum class ResetType : uint8_t
+  {
+    BROWN_OUT,
+    SOFTWARE,
+    HW_WATCHDOG_TIMEOUT,
+    UNKNOWN_RESET,
+    MAX_RESET_TYPE
+  };
 
-    enum class ResetType : uint8_t
-    {
-      BROWN_OUT,
-      SOFTWARE,
-      HW_WATCHDOG_TIMEOUT,
-      UNKNOWN_RESET,
-      MAX_RESET_TYPE
-    };
-
-    struct InterruptMask
-    {
-      bool interrupted;
-      uint32_t mask;
-    };
+  struct InterruptMask
+  {
+    bool interrupted;
+    uint32_t mask;
+  };
 }  // namespace Chimera::System
 
 #endif /* !CHIMERA_SYSTEM_TYPES_HPP */
