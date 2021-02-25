5d4e5ff6d4d3490c816179ab1687af222c53fdb7
Brandon Braun <brandonbraun653@gmail.com>
2021-02-24 23:22:15 -0600

    Add template class for declaring register mappings

---
+++ source/drivers/utilities/register_accessor.hpp
@@ -0,0 +1,164 @@
+/********************************************************************************
+ *  File Name:
+ *    register_accessor.hpp
+ *
+ *  Description:
+ *    Template metaprogramming for generating utility classes to define and
+ *    interact with hardware registers.
+ *
+ *  2021 | Brandon Braun | brandonbraun653@gmail.com
+ *******************************************************************************/
+
+#pragma once
+#ifndef CHIMERA_REGISTER_ACCESSOR_HPP
+#define CHIMERA_REGISTER_ACCESSOR_HPP
+
+/* STL Includes */
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#if defined( EMBEDDED )
+#define __IO volatile
+#else
+#define __IO
+#endif
+
+namespace Chimera::Gen
+{
+  /**
+   *  Register descriptor. This can apply to either an entire register or
+   *  a field inside a register.
+   *
+   *  @see RegAccess
+   *  @see FieldAccess
+   *
+   *  @param[in]  T         Base type that the register is sized for
+   *  @param[in]  Addr      Memory mapped address for the register
+   *  @param[in]  Mask      Mask that identifies which bits are accessible (default all)
+   *  @param[in]  Reset     Reset value for the register (default 0)
+   *  @param[in]  Offset    Bit position that the register starts at (default 0)
+   */
+  template<typename T, const T Addr, const T Mask = std::numeric_limits<T>::max(), const T Reset = 0, const T Offset = 0>
+  struct RegDecl
+  {
+    /*-------------------------------------------------
+    Assumptions & Checks
+    -------------------------------------------------*/
+    /* clang-format off */
+    static_assert( std::is_pod( T ),                        "Register type must be P.O.D". );
+    static_assert( Addr != static_cast<T>( 0 ),             "Register assigned to NULL. This is probably not true." );
+    static_assert( Addr % sizeof( T ) == 0,                 "Register address is not aligned to its underlying type." );
+    static_assert( Offset < std::numeric_limits<T>::digits, "Bit offset larger than what type can contain." );
+    static_assert( Mask != static_cast<T>( 0 ),             "Mask is zero. This disables all functionality!" );
+    /* clang-format on */
+
+    /*-------------------------------------------------
+    Public Fields
+    -------------------------------------------------*/
+    static constexpr __IO T *_accessor = static_cast<__IO T *>( Addr ); /**< Memory mapped address */
+    static constexpr T _address        = Addr;                          /**< Raw address value */
+    static constexpr T _bitOffset      = Offset;                        /**< Starting bit position for this field */
+    static constexpr T _bitMask        = Mask;                          /**< Bit mask for this field */
+    static constexpr T _resetValue     = Reset;                         /**< Value of the field upon reset */
+  };
+
+  /**
+   *  Register access helper functions
+   *
+   *  @param[in]  T         Base type that the register is sized for
+   *  @param[in]  Addr      Memory mapped address for the register
+   *  @param[in]  Mask      Mask that identifies which bits are accessible
+   *  @param[in]  Reset     Reset value for the register
+   *  @param[in]  Offset    Bit position that the register starts at (always zero)
+   */
+  template<typename T, const T Addr, const T Mask = std::numeric_limits<T>::max(), const T Reset = 0,
+           const T Offset = 0>
+  struct RegAccess : public RegDecl<T, Addr, Mask, Reset, Offset>
+  {
+    /*-------------------------------------------------------------------------------
+    Public Functions
+    -------------------------------------------------------------------------------*/
+    /**
+     *  Reads the current state of the register, masked appropriately
+     *  @return T
+     */
+    inline T read() const
+    {
+      return ( *_accessor ) & _bitMask;
+    }
+
+    /**
+     *  Assigns the entire register to the given value
+     *
+     *  @param[in]  val     Data to be assigned
+     *  @return void
+     */
+    inline void write( const T val )
+    {
+      *_accessor = val & _bitMask;
+    }
+
+    /**
+     *  Sets the entire register to zero
+     *  @return void
+     */
+    inline void clear()
+    {
+      write( 0 );
+    }
+
+    /**
+     *  Writes the reset value to the entire register
+     *  @return void
+     */
+    inline void reset()
+    {
+      write( _resetValue );
+    }
+
+    /**
+     *  Sets the bits in the given value, assuming it makes it through
+     *  the register mask.
+     *
+     *  @param[in]  val     Bit values to set
+     *  @return void
+     */
+    inline void bitwiseSet( const T val )
+    {
+      *_accessor |= val & _bitMask;
+    }
+
+    /**
+     *  Clears the bits that are set in the given value, if supported
+     *  by the register mask
+     *
+     *  @param[in]  val     Bit values to clear
+     *  @return void
+     */
+    inline void bitwiseClear( const T val )
+    {
+      *_accessor &= ~( val & _bitMask );
+    }
+  };
+
+
+  /**
+   *  Helper class to declare a register access type that describes a sub
+   *  field of an entire register.
+   *
+   *  @param[in]  Base      Base register declaration, of type RegAccess
+   *  @param[in]  Mask      Mask that identifies which bits belong to the field
+   *  @param[in]  Reset     Reset value for the field
+   *  @param[in]  Offset    Bit position that the field starts at
+   */
+  template<typename Base, const size_t Mask = std::numeric_limits<decltype( Base::_address )>::max(),
+           const decltype( Base::_address ) Reset = 0, decltype( Base::_address ) Offset = 0>
+  struct FieldAccess : public RegAccess<decltype( Base::_address ), Base::_address, Mask, Reset, Offset>
+  {
+  };
+
+}  // namespace Chimera::Gen
+
+#endif /* !CHIMERA_REGISTER_ACCESSOR_HPP */
